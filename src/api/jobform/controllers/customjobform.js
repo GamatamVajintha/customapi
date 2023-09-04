dccdf94e00905236a1039adba6d639cc47e33e54
GAMATAM VAJINTHA <gamatam.vajintha@zysk.tech>
2023-09-04 12:01:58 +0530

    forked files added

--- src/api/jobform/controllers/customjobform.js
+++ src/api/jobform/controllers/customjobform.js
@@ -3,18 +3,21 @@
 module.exports={
     async send(ctx){
         try{
-            const{name}=ctx.request.body
-            const nameRegex = /^[A-Za-z0-9\s]+$/;
+            const{name}=ctx.request.body.data
+            // const nameRegex = /^[A-Za-z0-9\s]+$/;
 
-            if (!name || !name.match(nameRegex)) {
+            if (!name) {
+                console.log(name)
                 ctx.status = 400;
-                ctx.body = { error: 'Name field is required' };
+                ctx.body = { error: "Name field is required" };
                 return;
             }
             // @ts-ignore
             const newName = await strapi.services.jobform.send({name})
+            console.log(strapi.services)
             ctx.status = 201; 
             ctx.body = newName;
+            
         }
         catch(error){
             ctx.throw(500,'An error occurred while processing your request.',error)
